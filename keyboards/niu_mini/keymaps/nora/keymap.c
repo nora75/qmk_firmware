5d05212ab2f61c765c578885bc97192363660c2f
nora75 <nora75@outlook.jp>
2018-09-14 18:28:46 +0900

    second keymaps

--- keyboards/niu_mini/keymaps/nora/keymap.c
+++ keyboards/niu_mini/keymaps/nora/keymap.c
@@ -3,7 +3,6 @@
 // readability
 #define _______  KC_TRNS
 #define XXXXXXX  KC_NO
-/* #define KC_GRAVE KC_GRV */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -13,16 +12,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+-------------+------+------+------+------+------|
 	 * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
-	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  '   |
+	 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Ctrl |  GUI | Caps |  Alt |Layer1|    Space    |Layer2| Left |  Up  | Down |Right |
+	 * |      |      |  GUI |  Alt |Layer1|    Space    |Layer2| Left |  Up  | Down |Right |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[0] = LAYOUT_planck_mit(
 		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
 		LCTL_T(KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-		KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(1),     KC_SPC,     MO(2), KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
+		KC_NO, KC_NO, KC_LGUI, KC_LALT, MO(1),     KC_SPC,     MO(2), KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
   ),
 
 	/* Layer 1
@@ -33,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * |      |      |      |      |      |      |      |      |      |      |      |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Reset|      |      |      |      |      |      |      |      |      |      |      |
+	 * |      |      |      |      |      |      |      |      |      |      |      |      |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[1] = LAYOUT_planck_mit(
 		KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
 		_______,   KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, KC_F11,  KC_F12,  _______, _______, _______,
 		_______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		RESET,     _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+		_______,     _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
 	),
 
 	/* Layer 2 (r_ Indicates RGB Controls)
@@ -72,3 +71,4 @@ void matrix_scan_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
 }
+    /* vim: set winwidth=120 */ 
