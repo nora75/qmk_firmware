7a68431dca0f4614978b708ac268141ade5924a1
nora75 <nora75@outlook.jp>
2018-11-07 15:35:34 +0900

    Add Tapping key,Change LEADER trigger,Add nora_tap.h,Change some keymap.

--- keyboards/niu_mini/keymaps/nora/keymap.c
+++ keyboards/niu_mini/keymaps/nora/keymap.c
@@ -1,13 +1,19 @@
 #include QMK_KEYBOARD_H
+#include "nora_tap.h"
 
-// readability
+/* readability */
 #define _______  KC_TRNS
 #define XXXXXXX  KC_NO
+
 /* static bool bsdel_mods = false; */
 
 /* enum custom_keycodes { */
 /*     M_BSDEL = SAFE_RANGE, */
 /* }; */
+
+#define DEFAULT 0
+#define LOWER 1
+#define UPPER 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -19,28 +25,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
      * |Sft_O_Sft |     Z    |     X    |     C    |     V    |     B    |     N    |     M    |     ,    |     .    |     /    |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
-     * |          |          |          |    Alt   |  Layer1  |        Space        |  Layer2  |    GUI   |          |          |          |
+     * |KC_NO-RES |          |          |    Alt   |  Layer1  |        Space        |  Layer2  |    GUI   |          |          |          |
      * `-----------------------------------------------------------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT_planck_mit(
-            KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-            LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
-            LSFT_T(OSM(MOD_LSFT)),        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-            KC_NO,          KC_NO,   KC_NO,   KC_LALT, MO(1),     KC_SPC,     MO(2), KC_LGUI, KC_NO,   KC_NO,   KC_NO
+    [DEFAULT] = LAYOUT_planck_mit(
+            KC_ESC,                KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+            LCTL_T(KC_TAB),        KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+            LSFT_T(OSM(MOD_LSFT)), KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+            TD(TD_NO_RES),         KC_NO,   KC_NO,   KC_LALT, MO(1),     KC_SPC,     MO(2), KC_LGUI, KC_NO,   KC_NO,   KC_NO
             ),
 
     /* Layer 1
      * ,-----------------------------------------------------------------------------------------------------------------------------------.
      * |     ~    |    F1    |    F2    |    F3    |    F4    |    F5    |    F6    |    F7    |    F8    |    F9    |    F10   |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
-     * |          |   Vol-   |   Vol+   |   Mute   |    F11   |   F12    |   Left   |   Down   |    Up    |   Right  |   LEAD   |          |
+     * |          |   Vol-   |   Vol+   |   Mute   |    F11   |   F12    |   Left   |   Down   |    Up    |   Right  |  LEADER  |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
      * |          |          |          |          |          |          |          |   Play   |          |          |          |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
      * |          |          |          |          |          |                     |          |          |          |          |          |
      * `-----------------------------------------------------------------------------------------------------------------------------------'
      */
-    [1] = LAYOUT_planck_mit(
+    [LOWER] = LAYOUT_planck_mit(
             KC_TILD,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
             _______,  KC_VOLD, KC_VOLU, KC_MUTE, KC_F11,  KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_LEAD, _______,
             _______,  _______, _______, _______, _______, _______, _______, KC_MPLY, _______, _______,  _______, _______,
@@ -51,24 +57,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------------------------------------------------------.
      * |     `    |     1    |     2    |     3    |     4    |     5    |     6    |     7    |     8    |     9    |     0    |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
-     * |          |   PgDn   |   PgUp   |   HOME   |    END   |          |     \    |     -    |     =    |     [    |     ]    |          |
+     * |          |   PgDn   |   PgUp   |   HOME   |    END   |          |     \    |     -    |     =    |  (-[-{   |   )-]-}  |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
      * |          |          |          |          |          |          |          |          |          |          |          |          |
      * |----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------+----------|
      * |          |          |          |          |          |                     |          |          |          |          |          |
      * `-----------------------------------------------------------------------------------------------------------------------------------'
      */
-    [2] = LAYOUT_planck_mit(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-            _______, KC_PGDN, KC_PGUP, KC_HOME,  KC_END, _______, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+    [UPPER] = LAYOUT_planck_mit(
+            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,           KC_0,           _______,
+            _______, KC_PGDN, KC_PGUP, KC_HOME,  KC_END, _______, KC_BSLS, KC_MINS, KC_EQL,  TD(TD_MY_LBRC), TD(TD_MY_RBRC), _______,
+            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______,        _______,
+            _______, _______, _______, _______, _______,     _______,      _______, _______, _______,        _______,        _______
             )
 };
 
 
-void matrix_init_user(void) {
-}
+/* void matrix_init_user(void) { */
+/* } */
+
+/* LEADER Functions */
+/* LEADER Key = <L> */
+/* <L>q : Alt(Meta) + F4 close current window(GUI) */
+
 
 LEADER_EXTERNS();
 
@@ -84,7 +95,7 @@ void matrix_scan_user(void) {
     /* SEQ_TWO_KEYS(KC_D, KC_D) { */
     /*   SEND_STRING(SS_LCTRL("a")SS_LCTRL("c")); */
     /* } */
-    SEQ_TWO_KEYS(KC_Q,KC_ENT) {
+    SEQ_ONE_KEY(KC_Q) {
       SEND_STRING(SS_LALT(SS_TAP(X_F4)));
     }
     /* SEQ_TWO_KEYS(KC_A, KC_S) { */
@@ -96,7 +107,7 @@ void matrix_scan_user(void) {
   }
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
     /* switch(keycode){ */
     /*     case M_BSDEL: */
     /*         if (record->event.pressed) { */
@@ -117,8 +128,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     /*         } */
     /* } */
 
-    return true;
-};
-
+    /* return true; */
+/* }; */
 
 /* vim: set nonu : */ 
---
+++ keyboards/niu_mini/keymaps/nora/nora_tap.h
@@ -0,0 +1,66 @@
+void dance_my_lbrc_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code(KC_LSFT);
+    register_code(KC_9);
+  } else if (state->count == 2) {
+    register_code(KC_LBRC);
+  } else {
+    register_code(KC_LSFT);
+    register_code(KC_LBRC);
+  }
+}
+
+void dance_my_lbrc_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code(KC_LSFT);
+    unregister_code(KC_9);
+  } else if (state->count == 2) {
+    unregister_code(KC_LBRC);
+  } else {
+    unregister_code(KC_LSFT);
+    unregister_code(KC_LBRC);
+  }
+}
+
+void dance_my_rbrc_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code(KC_LSFT);
+    register_code(KC_0);
+  } else if (state->count == 2) {
+    register_code(KC_RBRC);
+  } else {
+    register_code(KC_LSFT);
+    register_code(KC_RBRC);
+  }
+}
+
+void dance_my_rbrc_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code(KC_LSFT);
+    unregister_code(KC_0);
+  } else if (state->count == 2) {
+    unregister_code(KC_RBRC);
+  } else {
+    unregister_code(KC_LSFT);
+    unregister_code(KC_RBRC);
+  }
+}
+
+/* Tap Dance Keys Declarations */
+enum {
+  TD_NO_RES = 0,
+  TD_MY_LBRC,
+  TD_MY_RBRC
+};
+
+/* Tap Dance Definitions */
+qk_tap_dance_action_t tap_dance_actions[] = {
+  /* Tap once for NO, twice for RESET */
+  [TD_NO_RES]  = ACTION_TAP_DANCE_DOUBLE(KC_NO, RESET),
+  [TD_MY_LBRC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_my_lbrc_finished, dance_my_lbrc_reset),
+  [TD_MY_RBRC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_my_rbrc_finished, dance_my_rbrc_reset),
+ /* Other declarations would go here, separated by commas, if you have them */
+};
+
+
+/* vim: set nonu : */ 
