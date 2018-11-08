7ab94ab09c9d0b0eb58f301fd1aa0233cd46820d
nora75 <nora75@outlook.jp>
2018-11-08 14:30:17 +0900

    Reformat code

--- keyboards/niu_mini/keymaps/nora/keymap.c
+++ keyboards/niu_mini/keymaps/nora/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC,                KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_Y, KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC,
             LCTL_T(KC_TAB),        KC_A,    KC_S,    KC_D,    KC_F,      KC_G,   KC_H, KC_J,      KC_K,    KC_L,    KC_SCLN, KC_ENT,
             LSFT_T(OSM(MOD_LSFT)), KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_N, KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-            LT(SPECIAL, KC_NO),    KC_NO,   KC_NO,   KC_LALT, MO(LOWER),    KC_SPC,    MO(UPPER), KC_LGUI, KC_NO,   KC_NO,   KC_NO
+            MO(SPECIAL),           KC_NO,   KC_NO,   KC_LALT, MO(LOWER),    KC_SPC,    MO(UPPER), KC_LGUI, KC_NO,   KC_NO,   KC_NO
             ),
 
     /* Layer 1
