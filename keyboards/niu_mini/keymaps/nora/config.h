fec078c1154b89f008fdd3d9318ce6096d33ca3c
nora75 <nora75@outlook.jp>
2018-11-15 12:23:47 +0900

    Change Mouse key max speed to 7.

--- keyboards/niu_mini/keymaps/nora/config.h
+++ keyboards/niu_mini/keymaps/nora/config.h
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* mouse key options */
 #define MOUSEKEY_DELAY             300
 #define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_MAX_SPEED         7
 #define MOUSEKEY_TIME_TO_MAX       20
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
